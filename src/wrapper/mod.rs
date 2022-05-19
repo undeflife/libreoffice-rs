67c9772bac02f3653ebcd2e732b20898b27f0040
Sean Pan <orin.kakhan@gmail.com>
2022-05-19 14:56:06 +0800

    suppress warning for generated binding code

--- src/wrapper/mod.rs
+++ src/wrapper/mod.rs
@@ -1,3 +1,9 @@
+#![allow(
+    dead_code,
+    non_snake_case,
+    non_camel_case_types,
+    non_upper_case_globals
+)]
 mod bindings;
 
 #[link(name = "wrapper")]
@@ -22,8 +28,9 @@ pub struct Document {
 
 impl Office {
     pub fn new(install_path: &str) -> Office {
+        let c_install_path = CString::new(install_path).unwrap();
         unsafe {
-            let lok = lok_init_wrapper(CString::new(install_path).unwrap().as_ptr());
+            let lok = lok_init_wrapper(c_install_path.as_ptr());
             Office {
                 lok: lok,
                 lok_clz: (*lok).pClass,
@@ -38,33 +45,36 @@ impl Office {
     }
 
     pub fn document_load(&mut self, url: &str) -> Document {
+        let c_url = CString::new(url).unwrap();
         unsafe {
-            let doc = (*self.lok_clz).documentLoad.unwrap()(
-                self.lok,
-                CString::new(url).unwrap().as_ptr(),
-            );
+            let doc = (*self.lok_clz).documentLoad.unwrap()(self.lok, c_url.as_ptr());
             Document { doc: doc }
         }
     }
 
     pub fn document_load_with(&mut self, url: &str, options: &str) {
+        let c_url = CString::new(url).unwrap();
+        let c_options = CString::new(options).unwrap();
         unsafe {
             (*self.lok_clz).documentLoadWithOptions.unwrap()(
                 self.lok,
-                CString::new(url).unwrap().as_ptr(),
-                CString::new(options).unwrap().as_ptr(),
+                c_url.as_ptr(),
+                c_options.as_ptr(),
             );
         }
     }
 }
 impl Document {
     pub fn save_as(&mut self, url: &str, format: &str, filter: Option<&str>) {
+        let c_url = CString::new(url).unwrap();
+        let c_format: CString = CString::new(format).unwrap();
+        let c_filter: CString = CString::new(filter.unwrap_or_default()).unwrap();
         unsafe {
             (*(*self.doc).pClass).saveAs.unwrap()(
                 self.doc,
-                CString::new(url).unwrap().as_ptr(),
-                CString::new(format).unwrap().as_ptr(),
-                CString::new(filter.unwrap_or_default()).unwrap().as_ptr(),
+                c_url.as_ptr(),
+                c_format.as_ptr(),
+                c_filter.as_ptr(),
             );
         }
     }
